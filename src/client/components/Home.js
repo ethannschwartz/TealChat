16f760c9b58c64db35f7bbf267af7276793eb7d3
ethanschwartz <e.schwartz@seclock.com>
2022-03-08 07:20:55 +0200

    Made major adjustments

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,4 +1,3 @@
-// import ChatsSection from "./ChatsSection";
 import {useState} from "react";
 import Header from "./Header";
 import io from "socket.io-client";
@@ -6,11 +5,10 @@ import Chat from "./Chat";
 import NewChatModal from "./NewChatModal";
 
 const socket = io.connect('http://localhost:3001');
-console.log(socket);
 
 const Home = () => {
     const [newChatModalSeen, setNewChatModalSeen] = useState(false);
-    const [username, setUsername] = useState('');
+    const username = sessionStorage.getItem('username');
     const [room, setRoom] = useState('');
 
     const joinRoom = (e) => {
@@ -24,20 +22,19 @@ const Home = () => {
 
     return (
         <div className={'w-screen h-screen bg-teal-500 flex flex-col justify-center'}>
-            <Header setNewChatModalSeen={setNewChatModalSeen} newChatModalSeen={newChatModalSeen} />
+            <Header setNewChatModalSeen={setNewChatModalSeen}
+                    newChatModalSeen={newChatModalSeen}/>
+
             <section className={'flex bg-white w-full h-full max-w-2xl md:w-2/3 md:h-5/6 m-auto shadow-2xl'}>
                 { newChatModalSeen? <NewChatModal setNewChatModalSeen={setNewChatModalSeen}
                                                   socket={socket}
                                                   newChatModalSeen={newChatModalSeen}
                                                   joinRoom={joinRoom}
                                                   setRoom={setRoom}
-                                                  setUsername={setUsername}
                                                   room={room}
                                                   username={username}
 
                 />  : null }
-
-                {/*<ChatsSection setNewChatModalSeen={setNewChatModalSeen} newChatModalSeen={newChatModalSeen} />*/}
 
                 <Chat socket={socket}
                       username={username}
--- src/client/components/NewChatModal.js
+++ src/client/components/NewChatModal.js
@@ -4,21 +4,22 @@ const NewChatModal = (props) => {
     return (
         <div className={'w-screen h-screen backdrop-blur-sm bg-gray-700/75 fixed top-0 left-0 flex flex-col' +
             ' justify-center z-30'}>
-            <form className={'w-96 p-4 h-fit m-auto shadow-2xl bg-white rounded-md'}>
-                <button onClick={() => props.setNewChatModalSeen(false)} className={'text-slate-400 hover:text-black' +
-                    ' text-2xl float-right'}><MdClose /></button>
+            <form onClick={props.joinRoom}
+                  className={'w-96 p-4 h-fit m-auto shadow-2xl bg-white rounded-md'}>
+
+                <button onClick={() => props.setNewChatModalSeen(false)} className={'text-slate-400 hover:text-black text-2xl float-right'}>
+                    <MdClose />
+                </button>
+
                 <h1 className={'my-2 text-3xl text-slate-600 text-left'}>Create Chat</h1>
-                <input type="text"
-                       placeholder={'Username'}
-                       onChange={(e) => props.setUsername(e.target.value)}
-                       className={'p-2 my-2 border rounded-md my-2 w-full outline-none'}
-                />
+
                 <input type="text"
                        placeholder={'Room ID'}
                        onChange={(e) => props.setRoom(e.target.value)}
                        className={'p-2 my-2 border rounded-md my-2 w-full outline-none'}
                 />
-                <button onClick={props.joinRoom}
+
+                <button type={'submit'}
                         className={'bg-teal-600 text-white p-2 text-xl rounded-md w-full hover:bg-teal-700'}>Chat</button>
             </form>
         </div>
